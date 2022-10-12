64786991474c8f9818185dfb5fc4c07674054940
azeemkhan1994 <azeem.khan@pebtechsolutions.com>
2022-10-12 16:22:26 +0500

    init repo

---
+++ assets/js/captcha.js
@@ -0,0 +1,176 @@
+const key = '6Ldj7FwiAAAAAOQALqdIQbEWOBilDGc_527ho85r';
+
+let isHeaderForm = false;
+let isFooterForm = false;
+let isModelForm1 = false;
+let isModelForm2 = false;
+
+let HeaderCaptcha = null;
+let FooterCaptcha = null;
+let Modal1Captcha = null;
+let Modal2Captcha = null;
+
+let verifyHeaderCallback = function (response) {
+    if (response.length !== 0) {
+        isHeaderForm = true;
+        isFooterForm = false;
+        isModelForm1 = false;
+        isModelForm2 = false;
+        return
+    }
+};
+
+let verifyFooterCallback = function (response) {
+    if (response.length !== 0) {
+        isHeaderForm = false;
+        isFooterForm = true;
+        isModelForm1 = false;
+        isModelForm2 = false;
+        return
+    }
+};
+
+let verifyModal1Callback = function (response) {
+    if (response.length !== 0) {
+        isHeaderForm = false;
+        isFooterForm = false;
+        isModelForm1 = true;
+        isModelForm2 = false;
+        return
+    }
+};
+
+let verifyModal2Callback = function (response) {
+    if (response.length !== 0) {
+        isHeaderForm = false;
+        isFooterForm = false;
+        isModelForm1 = false;
+        isModelForm2 = true;
+        return
+    }
+};
+
+onloadCallback = function () {
+    HeaderCaptcha = grecaptcha.render('HeaderCaptcha', {
+        'sitekey': key,
+        'callback': verifyHeaderCallback,
+        'theme': 'light',
+    });
+
+    FooterCaptcha = grecaptcha.render('FooterCaptcha', {
+        'sitekey': key,
+        'callback': verifyFooterCallback,
+        'theme': 'light',
+    });
+
+    Modal1Captcha = grecaptcha.render('Modal1Captcha', {
+        'sitekey': key,
+        'callback': verifyModal1Callback,
+        'theme': 'light',
+    });
+    Modal2Captcha = grecaptcha.render('Modal2Captcha', {
+        'sitekey': key,
+        'callback': verifyModal2Callback,
+        'theme': 'light',
+    });
+};
+
+function formSubmitNow(form) {
+    if (form.checkValidity() === true) {
+        var formData = $(form).serialize();
+        $(form).find("button[type='submit']").attr({ disabled: true });
+        $.ajax({
+            type: "POST",
+            url: "https://pebtechsolutions.com/best-mobile-app-development-company-usa/send-email.php",
+            dataType: "json",
+            data: formData,
+            success: function (response) {
+                if (response.success) {
+                    form.reset();
+                    location.replace("https://pebtechsolutions.com/thankyou.html");
+                } else { }
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr);
+            },
+        });
+    }
+}
+
+$("#headerForm").on('submit', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (isHeaderForm === true && isFooterForm === false && isModelForm1 === false && isModelForm2 === false) {
+        formSubmitNow(this);
+    } else {
+        alert('Please Fill the Captcha');
+    }
+});
+
+$("#FooterForm").on('submit', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (isHeaderForm === false && isFooterForm === true && isModelForm1 === false && isModelForm2 === false) {
+        formSubmitNow(this);
+    } else {
+        alert('Please Fill the Captcha');
+    }
+});
+
+$("#ModalForm1").on('submit', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (isHeaderForm === false && isFooterForm === false && isModelForm1 === true && isModelForm2 === false) {
+        formSubmitNow(this);
+        return
+    } else {
+        alert('Please Fill the Captcha');
+    }
+});
+
+$("#ModalForm2").on('submit', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (isHeaderForm === false && isFooterForm === false && isModelForm1 === false && isModelForm2 === true) {
+        formSubmitNow(this);
+        return
+    } else {
+        alert('Please Fill the Captcha');
+    }
+});
+
+// // Fetch all the forms
+// const forms = document.querySelectorAll(".contactForm");
+
+// // Loop over them
+// Array.from(forms).forEach((form) => {
+//     form.addEventListener(
+//         "submit",
+//         (event) => {
+//             event.preventDefault();
+//             event.stopPropagation();
+
+//             if (form.checkValidity() === true) {
+//                 var formData = $(form).serialize();
+//                 $.ajax({
+//                     type: "POST",
+//                     url: "https://pebtechsolutions.com/demowebsite/best-mobile-app-development-company-usa/send-email.php",
+//                     dataType: "json",
+//                     data: formData,
+//                     success: function (response) {
+//                         if (response.success) {
+//                             form.reset();
+//                             location.replace("https://pebtechsolutions.com/thankyou.html");
+//                         } else { }
+//                     },
+//                     error: function (xhr, status, error) {
+//                         console.log(xhr);
+//                     },
+//                 });
+//             }
+
+//             return false;
+//         },
+//         false
+//     );
+// });
---
+++ send-email.php
@@ -0,0 +1,91 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+$mailUsr = new PHPMailer(true);                      // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'relay-hosting.secureserver.net';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = false;                               // Enable SMTP authentication
+    $mail->Username = '';                 // SMTP usernme
+    $mail->Password = '';                           // SMTP password
+    $mail->SMTPSecure = 'none';
+    $mail->Port = 25;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('info@pebtechsolutions.com');
+    // $mail->addAddress('rashid.latif@pebtechsolutions.com');     // Add a recipient
+    // $mail->addAddress('khurram.khan@pebtechsolutions.com');
+    $mail->addAddress('azeem.khan@pebtechsolutions.com');     // Add a recipient
+
+
+
+    //Content
+    // Set email format to HTML
+
+    $name = $_POST['Name'];
+    $email = $_POST['Email'];
+    $country = $_POST['ctry'];
+    $phone = $_POST['Phone'];
+    $message = $_POST['Comment'];
+    $target = $_POST['Target'];
+
+    $emailbody = "Name = " . $name . "<br>Email = " . $email . "<br>Country = " . $country . "<br>Phone = " .  $phone .  "<br>Message =" . $message . "<br> Location =" . $target;
+
+    $mail->Subject = "PEBTECHSOLUTIONS Landing Page - CONTACT USER DETAILS!";
+    $mail->Body    =     $emailbody;
+
+    $mail->isHTML(true);
+    $mail->send();
+
+    //=========================For User===========================//
+
+    //Server settings
+    $mailUsr->SMTPDebug = 0;                                 // Enable verbose debug output
+    $mailUsr->isSMTP();                                      // Set mailer to use SMTP
+    $mailUsr->Host = 'relay-hosting.secureserver.net';  // Specify main and backup SMTP servers
+    $mailUsr->SMTPAuth = false;                               // Enable SMTP authentication
+    $mailUsr->Username = '';                 // SMTP usernme
+    $mailUsr->Password = '';                           // SMTP password
+    $mailUsr->SMTPSecure = 'none';
+    $mailUsr->Port = 25;                                    // TCP port to connect to
+
+    //Recipients
+    $mailUsr->setFrom('info@pebtechsolutions.com');
+    $mailUsr->addAddress($email);     // Add a recipient
+
+
+
+    //Content
+    // Set email format to HTML
+
+    $emailbodyUsr =  "<img src='https://pebtechsolutions.com/demowebsite/LOGO-FOR-EMAIL-SIGNATURE.png' height='91' width='203' /> <br/><br/>  
+    Hi <b>" . $name . "</b><br/><br/>" .
+        "Thank you for visiting our website. <br/><br/>
+    We would like to take this opportunity to introduce PebTech Solutions a one-stop place for all IT-related services to fulfil your business needs.<br/>
+    We specialize in Android and IOS App development, Web Development, Logo Design etc.PebTech uses latest technologies to deliver digital solutions.<br/><br/>
+    Kindly share your availability so that we shall discuss the work scope of your project and the required deliverables.<br/>
+    <a href='https://calendly.com/pebtechsolutions'> Let's Schedule meeting</a><br/><br/>
+    Waiting for your feedback.<br/><br>
+    Warm Regards<br/>
+    Support Team";
+
+    $mailUsr->Subject = "PEBTECHSOLUTIONS - THANK YOU FOR YOUR CONTACT!";
+    $mailUsr->Body    =     $emailbodyUsr;
+
+    $mailUsr->isHTML(true);
+    $mailUsr->send();
+
+    echo json_encode(['success' => true]);
+    exit();
+} catch (Exception $e) {
+    echo json_encode(['success' => false]);
+}
