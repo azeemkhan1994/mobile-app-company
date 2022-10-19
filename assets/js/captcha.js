6b9a0ef17b696bf9788be761298c262fee166ff0
azeemkhan1994 <azeem.khan@pebtechsolutions.com>
2022-10-19 18:55:10 +0500

    change thank you link

--- assets/js/captcha.js
+++ assets/js/captcha.js
@@ -87,7 +87,7 @@ function formSubmitNow(form) {
             success: function (response) {
                 if (response.success) {
                     form.reset();
-                    location.replace("https://pebtechsolutions.com/thankyou.html");
+                    location.replace("https://pebtechsolutions.com/thank-you.html");
                 } else { }
             },
             error: function (xhr, status, error) {
